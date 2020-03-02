1f6d69112a1cc560cf4468837bdd8a68c955165c
Paul Rosero <paul_rosero@yahoo.com>
2020-03-01 19:34:08 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+let pi = Math.PI
+
+class Circle {
+    constructor(radius) {
+        this.radius = radius
+    }
+   
+    get diameter() {
+        return this.radius * 2
+    }
+
+    get circumference() {
+        return pi * (this.radius * 2)
+    }
+
+    get area() {
+        return (this.radius * this.radius) * pi
+    }
+
+    set diameter(diameter) {
+        this.radius = diameter / 2
+    }
+
+    set circumference(circumference) {
+        this.radius = circumference / (pi * 2)
+    }
+}
