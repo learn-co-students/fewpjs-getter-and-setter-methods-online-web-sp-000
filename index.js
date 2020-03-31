cd4eb6135cf3372763ab36f0989975ce1a1358b3
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-31 00:44:15 +0000

    Done.

--- index.js
+++ index.js
@@ -4,25 +4,22 @@ class Circle {
   }
 
   get diameter() {
-    return this._diameter = this.radius * 2
+    return this.radius * 2
   }
 
   set diameter(diameter) {
-    this._diameter = diameter
-    this.radius = this._diameter / 2
+    this.radius = diameter / 2
   }
 
   get circumference() {
-    return this._circumference = Math.PI * this.diameter
-    console.log(pi * this.diameter)
+    return Math.PI * this.diameter
   }
 
   set circumference(circumference) {
-    this._circumference = circumference
-    this.radius = (this._circumference / Math.PI) / 2
+    this.radius = circumference / (Math.PI * 2)
   }
 
   get area() {
-    return this._area = Math.PI * (this.radius ** 2)
+    return Math.PI * (this.radius ** 2)
   }
 }
