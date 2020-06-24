e6638289538afcf0e8fb081bb5df7d6dccee6f77
juan siu <juan.siu@gmail.com>
2020-06-24 00:49:01 +0000

    Done.

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ class Circle {
     return Math.PI * (this.radius * this.radius);
   }
   set diameter(diameter){
-    this.diameter = diameter;
-    this.radius = diameter / 2;
+  //  this.diameter = diameter;
+     this.radius = diameter / 2;
   }
   set circumference(circumference){
-    this.circumference = circumference;
-    this.radius = (circumference * 2) / Math.PI;
+  //  this.circumference = circumference;
+     this.radius = circumference / (Math.PI * 2);
   }
   set area(area){
-    this.area = area;
-    this.radius = Math.sqrt(area / Math.PI);
+  //  this.area = area;
+     this.radius = Math.sqrt(area / Math.PI);
   }
 }
