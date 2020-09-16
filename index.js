bc3364c3e6728aae27c735e6cead6ddc5b38c51b
connorworthen <connorw220@protonmail.com>
2020-09-15 22:33:04 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Add your Circle class here
+function circle(radius) {
+  this.radius = radius;
+  // area method
+  this.area = function () {
+    return Math.PI * this.radius * this.radius;
+  };
+  // perimeter method
+  this.perimeter = function () {
+    return 2 * Math.PI * this.radius;
+  };
+}
